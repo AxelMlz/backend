e12a55923382fe97d0938e741cec91be6a7b5229
AxelMlz <axel.fm.melezan@gmail.com>
2022-03-08 16:10:16 +0100

    ch2_students_end

--- ch2/heroes/index.js
+++ ch2/heroes/index.js
@@ -2,32 +2,32 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-var students = [
-    {
-        name: "Lawrence",
-        lastname: "Nowell",
-        nationality: "UK",
-    },
-    {
-        name: "William",
-        lastname: "Shakespeare",
-        nationality: "UK",
-    },
-    {
-        name: "Charles",
-        lastname: "Dickens",
-        nationality: "US",
-    },
-    {
-        name: "Oscar",
-        lastname: "Wilde",
-        nationality: "UK",
-    },
-]
+// var students = [
+//     {
+//         name: "Lawrence",
+//         lastname: "Nowell",
+//         nationality: "UK",
+//     },
+//     {
+//         name: "William",
+//         lastname: "Shakespeare",
+//         nationality: "UK",
+//     },
+//     {
+//         name: "Charles",
+//         lastname: "Dickens",
+//         nationality: "US",
+//     },
+//     {
+//         name: "Oscar",
+//         lastname: "Wilde",
+//         nationality: "UK",
+//     },
+// ]
 
-app.get ("/" &&"/students",(req,res)=> {
-    res.send("Students API")
-})
+// app.get ("/" &&"/students",(req,res)=> {
+//     res.send("Students API")
+// })
 
 app.listen(port, () => {
   console.log('Server started on port: ' + port);
