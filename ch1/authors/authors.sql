ed151d14148d036a5d1118985461a4199db954d0
AxelMlz <axel.fm.melezan@gmail.com>
2022-03-21 16:03:31 +0100

    implementing sql server in ch1

--- ch1/authors/authors.sql
+++ ch1/authors/authors.sql
@@ -2,9 +2,9 @@
 -- CREATE SCHEMA PUBLIC;
 
 CREATE TABLE authors(
-	student_id SERIAL PRIMARY KEY,
+	author_id SERIAL PRIMARY KEY,
 	name VARCHAR(30),
-	nationality VARCHAR(30),
+	nationality VARCHAR(10),
 	books VARCHAR(100)
 );
 
@@ -13,53 +13,9 @@ INSERT INTO authors(name, nationality, books) VALUES('William Shakespeare', 'UK'
 INSERT INTO authors(name, nationality, books) VALUES('Charles Dickens', 'US', '"Oliver Twist", "A Christmas Carol"');
 INSERT INTO authors(name, nationality, books) VALUES('Oscar Wilde', 'UK', '"The Picture of Dorian Gray", "The Importance of Being Earnest"');
 
-
 SELECT * FROM authors;
+SELECT * FROM authors WHERE authors.author_id=$1;
 
-CREATE TABLE favorites(
-	favorite_id SERIAL PRIMARY KEY,
-	class VARCHAR(30),
-	sport VARCHAR(30),
-	student_id INTEGER,
-	CONSTRAINT fk_student FOREIGN KEY(student_id) REFERENCES students(student_id)
-);
-
-INSERT INTO favorites(class, sport, student_id) VALUES
-('Maths', 'Cricket', 2),
-('Music', 'Hip-hop', 6),
-('Arts', 'Boxing', 1),
-('Literature', 'Tennis', 3),
-('Science', 'Tennis', 5),
-('Arts', 'Baseball', 4);
-
-CREATE TABLE students_languages(
-	id SERIAL PRIMARY KEY,
-	student_id INTEGER,
-	language_id INTEGER,
-	CONSTRAINT fk_student FOREIGN KEY(student_id) REFERENCES students(student_id),
-	CONSTRAINT fk_language FOREIGN KEY(language_id) REFERENCES languages(language_id)
-);
-
-/* Rapport lvl 1 */
-SELECT * FROM students WHERE student_id=3;
-SELECT * FROM students WHERE student_id=6;
-SELECT name, city FROM students WHERE student_id=1;
-SELECT name FROM students WHERE student_id=2;
-SELECT * FROM students WHERE city='Paris';
-SELECT name FROM students WHERE city='Lyon';
-
-/* Rapport lvl 2 */
-SELECT * FROM students
-INNER JOIN favorites ON students.student_id=favorites.student_id
-WHERE students.student_id=5;
-
-SELECT name, sport FROM students
-INNER JOIN favorites ON students.student_id=favorites.student_id
-WHERE students.student_id=4;
-
-SELECT name, class FROM students
-INNER JOIN favorites ON students.student_id=favorites.student_id
-WHERE students.student_id=1;
 
 
 
--- ch1/index.js
+++ ch1/index.js
@@ -6,43 +6,62 @@ dotenv.config({
 	path: "./config.env",
 });
 const { Pool } = require("pg");
+const Postgres = new Pool({ ssl: { rejectUnauthorized: false } });
 
-var authors = [
-    {
-        name: "Lawrence Nowell",
-        nationality: "UK",
-        books: ["Beowulf"]
-    },
-    {
-        name: "William Shakespeare",
-        nationality: "UK",
-        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
-    },
-    {
-        name: "Charles Dickens",
-        nationality: "US",
-        books: ["Oliver Twist", "A Christmas Carol"]
-    },
-    {
-        name: "Oscar Wilde",
-        nationality: "UK",
-        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
-    },
-]
+// var authors = [
+//     {
+//         name: "Lawrence Nowell",
+//         nationality: "UK",
+//         books: ["Beowulf"]
+//     },
+//     {
+//         name: "William Shakespeare",
+//         nationality: "UK",
+//         books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+//     },
+//     {
+//         name: "Charles Dickens",
+//         nationality: "US",
+//         books: ["Oliver Twist", "A Christmas Carol"]
+//     },
+//     {
+//         name: "Oscar Wilde",
+//         nationality: "UK",
+//         books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+//     },
+// ]
 
-app.get ("/" &&"/authors",(req,res)=> {
-    res.send("Authors API")
-})
+app.get ("/authors", async (req,res)=> {
+    let authors;
+	try {
+		authors = await Postgres.query("SELECT * FROM authors");
+	} catch (err) {
+		console.log(err);
 
-app.get ("/authors/:id",(req,res)=> {
-    const id = authors[parseInt(req.params.id)-1];
-   let whoAuthors= res.json( `the author is ${id.name}, from ${id.nationality}`);
+		return res.status(400).json({
+			message: "An error happened",
+		});
+	}
 
-if(!id){
+	res.json(authors.rows);
+});
+
+//     res.send("Authors API")
+// })
+
+app.get ("/authors/:id", async (req,res)=> {
+    // const id = authors[parseInt(req.params.id)-1];
+    const authors = await Postgres.query(
+        "SELECT * FROM authors WHERE authors.authors_id=1", [req.params.id]
+    );
+    res.json(authors.rows)
+//    let whoAuthors= res.json( `the author is ${id.name}, from ${id.nationality}`);
+
+if(!req.params.id){
     res.json({message:`this author do not exist`})
-}
-res.send(whoAuthors);
-})
+}})
+// res.send(whoAuthors);
+// 
 
 app.get ("/authors/:id/books",(req,res)=> {
     const id = authors[parseInt(req.params.id)-1];
